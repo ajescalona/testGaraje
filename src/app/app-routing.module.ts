a217043d9640b4ca8a298ddcf664a0f5d30abfab
Renan Braguim de Souza <renan.braguim@qaracter.es>
2019-11-27 10:47:38 +0100

    firts commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeTestComponent } from './home-test/home-test.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'homeTest',
+    component: HomeTestComponent,
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
